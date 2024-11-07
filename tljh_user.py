eaee25942b9f5a8c1f803c2bcd042e7b7671c61a
Victor Olet <55980431+victorolet@users.noreply.github.com>
2024-11-07 22:29:37 +0800

    Update tljh_user.py

--- tljh_user.py
+++ tljh_user.py
@@ -1,6 +1,11 @@
 from tljh.hooks import hookimpl
 
 @hookimpl
+def tljh_post_install():
+    with open("test_tljh_post_install", "w") as f:
+        f.write("file_written_by_simplest_plugin")
+
+@hookimpl
 def tljh_new_user_create(username):
     with open("test_new_user_create", "w") as f:
         f.write("file_written_by_simplest_plugin")
